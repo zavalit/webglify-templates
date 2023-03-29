9c23555d68bcb332de3fd38adc4402ca2b0406ff
Vsevolod (Seva) Dolgopolov <zavalit@gmail.com>
2023-03-29 12:29:49 +0200

    add mouse to unform to default

--- templates/default/shaders/fragment.glsl
+++ templates/default/shaders/fragment.glsl
@@ -4,12 +4,16 @@ precision mediump float;
 
 uniform float uTime;
 uniform vec2 uResolution;
+uniform vec2 uMouse;
 
 out vec4 fragColor;
 
 void main() {
   vec2 uv = gl_FragCoord.xy / uResolution;
-  float time = fract(uTime);
+  float time = mod(uTime, 2.);
+  time = time > 1. ? 2. - time : time;
+  vec2 mouse = uMouse / uResolution;
+  uv += mouse;
 
   fragColor = vec4(uv, time, 1.0);
 }
